7b5472d940b308b8998184843090ffa99390b4fa
amanda1304 <mand.amanda1304@gmail.com>
2025-03-24 21:25:07 -0300

    #1-enviado alterações na tela de long/cadrastro

---
+++ Login_Register.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login_Register
+{
+    public partial class Login_Register: Form
+    {
+        public Login_Register()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMinimizarRegister_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnFecharRegister_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtRegister_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panelconfirmsenha_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void txtConfirmarSenha_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panelsenharegister_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void txtSenhaRegister_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLogin_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+
+        private void panelemail_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtJaTemConta_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtEmail_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtUserNameRegister_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxEmail_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxSenhaRegister_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnFecharLogin_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMinimizarLogin_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void linkRegister_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+
+        private void pnl_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void txtLogin_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
