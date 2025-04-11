a01c4c2993513d26dc7cc31fd495cbd174852e72
amanda1304 <mand.amanda1304@gmail.com>
2025-04-11 14:42:12 -0300

    m

--- Model/Configuracoes/ConfiguracoesTema.cs
+++ Model/Configuracoes/ConfiguracoesTema.cs
@@ -8,5 +8,9 @@ namespace Login_Register.Model.Configuracoes
 {
     internal class ConfiguracoesTema
     {
+        public int id { get; set; }
+        public string cor_tema { get; set; }
+        public string outros_parametros { get; set; }
+        public string avatar_perfil { get; set; }
     }
 }
